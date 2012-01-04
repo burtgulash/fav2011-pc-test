ad57f982cd2478861ad57967e4105bdfaead89ff
gusta <burtgulas@gmail.com>
2012-01-04 17:48:15 +0100

    posledni

---
+++ test10.c
@@ -0,0 +1,8 @@
+/**
+ * K čemu slouží knihovní fuknce fflush?
+ * Uveďte příklad.
+ **/
+
+/* Moje odpověď: Vypíše obsah bufferu. Vhodný, když se třeba chci ujisti, že
+ * to mam všechno zapsaný na disku. 
+ */
